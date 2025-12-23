69d688db088047c398e2e37e7bb4ce297596fde9
McCoy1002 <mccoy1002@163.com>
2025-12-23 17:21:50 +0800

    搭建基础路由和组件

---
+++ lib/routes/index.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Login/index.dart';
+import 'package:hm_shop/pages/Main/index.dart';
+
+// 返回App根级组件
+Widget getRootWidget() {
+  return MaterialApp(
+    // 初始路由
+    initialRoute: "/list",
+    // 路由表
+    routes: getRootRoutes(),
+  );
+}
+
+// 根路由表
+Map<String, Widget Function(BuildContext)> getRootRoutes() {
+  return {
+    "/": (context) => MainPage(), // 首页
+    "/login": (context) => LoginPage(), // 登录页
+  };
+}
